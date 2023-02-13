6a9181d297d9277a44faef362374793e71748102
Vini Pereira <vini@vinipereira.io>
2023-02-13 20:28:15 +1100

    finish adding about content

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,9 @@
 function About() {
     return (
-        <h1>This is the about section</h1>
+        <div>
+            <h2>About</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        </div>
     )
 }
 
