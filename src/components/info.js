7907016c9bcad264558b417463372f982a3f8dda
Vini Pereira <vini@vinipereira.io>
2023-02-13 20:26:13 +1100

    finish adding info content

--- src/components/info.js
+++ src/components/info.js
@@ -4,6 +4,13 @@ function Info() {
     return (
         <div>
             <img src={ProfileImage}></img>
+            <h1>Laura Smith</h1>
+            <h3>Frontend Developer</h3>
+            <p>laurasmith.website</p>
+            <div>
+                <button>Email</button>
+                <button>LinkedIn</button>
+            </div>
         </div>
     )
 }
