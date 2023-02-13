89d4b8daebc49a73a770bd2625d2ac51142d5197
Vini Pereira <vini@vinipereira.io>
2023-02-13 19:54:06 +1100

    structure components import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Info from '../components/info.js';
+import About from '../components/about.js';
+import Interests from '../components/interests.js';
+import Footer from '../components/footer.js';
 
 function App() {
   return (
     <div className="App">
-
+      < Info/>
+      < About/>
+      < Interests/>
+      < Footer/>
     </div>
   );
 }
