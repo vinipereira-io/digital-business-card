fef00a072bc54b76f3b5becf58aee5df98b38b3a
Vini Pereira <vini@vinipereira.io>
2023-02-14 17:33:53 +1100

    add styles to the info section

--- src/components/info.js
+++ src/components/info.js
@@ -2,14 +2,14 @@ import ProfileImage from '../profileImage.png';
 
 function Info() {
     return (
-        <div>
+        <div className='Info'>
             <img src={ProfileImage}></img>
             <h1>Laura Smith</h1>
             <h3>Frontend Developer</h3>
             <p>laurasmith.website</p>
-            <div>
-                <button>Email</button>
-                <button>LinkedIn</button>
+            <div className='Info--buttons'>
+                <button data-destination='email'>Email</button>
+                <button data-destination='linkedin'>LinkedIn</button>
             </div>
         </div>
     )
