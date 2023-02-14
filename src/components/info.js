97a0d4502e07d9b2ed592934e44a047038f3ce7b
Vini Pereira <vini@vinipereira.io>
2023-02-14 17:55:44 +1100

    adjust styles of the info section

--- src/components/info.js
+++ src/components/info.js
@@ -4,12 +4,14 @@ function Info() {
     return (
         <div className='Info'>
             <img src={ProfileImage}></img>
-            <h1>Laura Smith</h1>
-            <h3>Frontend Developer</h3>
-            <p>laurasmith.website</p>
-            <div className='Info--buttons'>
-                <button data-destination='email'>Email</button>
-                <button data-destination='linkedin'>LinkedIn</button>
+            <div className='Info--content'>
+                <h1>Laura Smith</h1>
+                <h3>Frontend Developer</h3>
+                <p>laurasmith.website</p>
+                <div className='Info--content--buttons'>
+                    <button data-destination='email'>Email</button>
+                    <button data-destination='linkedin'>LinkedIn</button>
+                </div>
             </div>
         </div>
     )
