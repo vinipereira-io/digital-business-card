f7fcdafa3253d71d21a1cfa39e3db3d06a3c22b3
Vini Pereira <vini@vinipereira.io>
2023-02-15 23:01:54 +1100

    add icons from fontawesome

--- src/components/info.js
+++ src/components/info.js
@@ -10,10 +10,10 @@ function Info() {
                 <a href='https://vinipereira.io' target='_blank'>vinipereira.io</a>
                 <div className='Info--content--buttons'>
                     <form action='mailto:vini@vinipereira.io' method='get'>
-                        <button data-destination='email'>Email</button>
+                        <button data-destination='email'><i class="fa-solid fa-envelope"></i> Email</button>
                     </form>
                     <form action='https://www.linkedin.com/in/viniciusvmp/' method='get' target='_blank'>
-                        <button data-destination='linkedin'>LinkedIn</button>
+                        <button data-destination='linkedin'><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
                     </form>
                 </div>
             </div>
